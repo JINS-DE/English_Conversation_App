5c4fa3ea8680d08b05e279eae26e6089e66a9a2e
JINS-DE <rlawlstjd044@naver.com>
2024-05-13 14:38:45 +0900

    push

--- English_conversation_app/asgi.py
+++ English_conversation_app/asgi.py
@@ -1,21 +1,8 @@
-# import os
-# from channels.routing import ProtocolTypeRouter
-# from django.core.asgi import get_asgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'English_conversation_app.settings')
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-# })
-
-
-# asgi.py
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  # your_app은 웹소켓 경로를 정의할 앱 이름으로 변경
+import chat.routing  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'English_conversation_app.settings')
 
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,33 +1,31 @@
-# import json
-# from channels.generic.websocket import AsyncWebsocketConsumer
-
-# class ChatConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         await self.accept()
-
-#     async def disconnect(self, close_code):
-#         pass
-
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json['message']
-
-#         # 메시지를 받았을 때 필요한 처리를 수행합니다. 예를 들면:
-#         # 여기서 ChatGPT에 메시지를 보내고 응답을 받는 API 호출을 할 수 있습니다.
-
-#         # 클라이언트로 메시지 보내기
-#         await self.send(text_data=json.dumps({
-#             'message': message
-#         }))
-
-
-
-
-
 
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # 기존 코드 유지
+        await super().connect()
+
+    async def disconnect(self, close_code):
+        # 기존 코드 유지
+        await super().disconnect()
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # ChatGPT API를 호출하여 응답을 받습니다.
+        response_message = await self.chat_with_gpt(message)
+
+        # 응답을 모든 클라이언트에 전송
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': response_message
+            }
+        )
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
