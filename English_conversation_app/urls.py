c7671ad0c9ee24f9ed54bb7a61b666e05bafb0e7
JINS-DE <rlawlstjd044@naver.com>
2024-05-13 20:48:57 +0900

    +static_url

--- English_conversation_app/urls.py
+++ English_conversation_app/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path('',include('users.urls')),
 ] 
 
-# + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
++ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
